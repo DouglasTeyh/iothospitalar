61a68b47a7aa4000a3723e4003fc1498165d33e2
DouglasTeyh <contato.douglasteyh@gmail.com>
2025-05-06 08:11:27 -0300

    correção de bugs #3

--- scripts/navs-loader.js
+++ scripts/navs-loader.js
@@ -1,10 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("components/nav-principal.html")
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById("nav-principal-placeholder").innerHTML = data;
-        });
-
     fetch("components/nav.html")
         .then(res => res.text())
         .then(data => {
